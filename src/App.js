c9ad60c9cc7b73c859c73dc62df20dba9c76c6c6
AndresHelloWorld <andresangg@gmail.com>
2022-09-07 17:47:02 -0500

    fix2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import { Button } from '@material-ui/core'
 
 function App() {
+  const [fileValue, setFileValue] = useState();
+  const [rs, setRs] = useState([]);
+
+  function createAccidente(fecha, placa, nV, comentario) {
+    return { fecha, placa, nV, comentario };
+  }
+  var roow = []
+  const fileReader = (e) => {
+    const arch = e.target.files[0]
+    const fileR = new FileReader();
+    fileR.readAsText(arch)
+    fileR.onload = () => {
+      var format = fileR.result.split('\n');
+      setFileValue(format)
+    }
+    fileR.onerror = () => {
+      console.log(fileR.error)
+    }
+  }
+  const filter = () => {
+    const regex = /[^a-z|A-Z](moto)[^r]/g;
+    Object.keys(fileValue).map(key => {
+      if (regex.test(fileValue[key])) {
+        var doc = document.getElementById('res')
+        console.log(typeof fileValue[key], fileValue[key])
+        setRs(rs)
+        // doc.innerHTML += <div> <h1></h1><br /> </div>
+      }
+      // setRs(fileValue[key].replaceAll(';', "     "))
+
+    }
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Pruebas</h2>
+      <h2>{fileValue}</h2>
+      <input
+        type="file"
+        multiple={false}
+        onChange={fileReader}
+      />
+      <br />
+      <br />
+      <Button variant="contained" color="primary" onClick={filter}>Filtrar</Button>
+      <div id="res">{rs}</div>
+      <textarea id=""></textarea>
+
     </div>
+
   );
 }
-
 export default App;
